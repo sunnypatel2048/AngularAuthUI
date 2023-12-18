22981372bc3494e27bce74d1de8b2cb4b5f8ed33
Sunny Patel <sunnypatel2048@gmail.com>
2023-12-18 21:44:00 +0530

    Added UI validations for login & signup forms

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import ValidateForm from '../../helpers/validateform';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -12,11 +15,33 @@ export class LoginComponent {
 
   type: string = "password";
   isText: boolean = false;
-  eyeIcon: string = "fa-eye-slash"
+  eyeIcon: string = "fa-eye-slash";
+  loginForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
 
   hideShowPass(): void {
     this.isText = !this.isText;
     this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash"
     this.isText ? this. type = "text" : this.type = "password"
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      // Send data to db
+      console.log(this.loginForm.value)
+    }
+    else {
+      // throw error
+      ValidateForm.validateAllFormFields(this.loginForm);
+      alert("Form is invalid");
+    }
   }
 }
--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,21 +1,49 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import ValidateForm from '../../helpers/validateform';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink, ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
   type: string = "password";
   isText: boolean = false;
-  eyeIcon: string = "fa-eye-slash"
+  eyeIcon: string = "fa-eye-slash";
+  signUpForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.signUpForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    })
+  }
 
   hideShowPass(): void {
     this.isText = !this.isText;
     this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash"
     this.isText ? this. type = "text" : this.type = "password"
+  }
+
+  onSignUp(): void {
+    if (this.signUpForm.valid) {
+      // Send data to db
+      console.log(this.signUpForm.value)
+    }
+    else {
+      // throw error
+      ValidateForm.validateAllFormFields(this.signUpForm);
+      alert("Form is invalid");
+    }
   }
 }
